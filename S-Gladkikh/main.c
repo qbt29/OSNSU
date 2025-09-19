b999d6069cb3098e23110a7309cc0a0b622c238f
Sergey Gladkikh <s.gladkikh1@g.nsu.ru>
2025-09-19 17:31:36 +0700

    UID and GID

--- S-Gladkikh/main.c
+++ S-Gladkikh/main.c
@@ -1,5 +1,51 @@
 #include "stdio.h"
+#include "getopt.h"
+#include "stdlib.h"
+#include "string.h"
+struct GUID{
+    int uid;
+    int euid;
+    int gid;
+    int egid;
+};
 
-int main(void){
-    printf("Hello, World!");
+struct GUID getGUID(void){
+    struct GUID out;
+    FILE* f = fopen("/proc/self/status","r");
+    char buf[255]={'\0'};
+    char uid_s[16], euid_s[16], gid_s[16], egid_s[16];
+    while(strcmp(buf,"Uid:")!=0){
+        fscanf(f,"%s",buf);
+    }
+    fscanf(f,"%s",uid_s);
+    fscanf(f,"%s",euid_s);
+    while(strcmp(buf,"Gid:")!=0){
+        fscanf(f,"%s",buf);
+    }
+    fscanf(f,"%s",gid_s);
+    fscanf(f,"%s",egid_s);
+    fclose(f);
+    out.uid=atoi(uid_s);out.euid=atoi(euid_s);out.gid=atoi(gid_s);out.egid=atoi(egid_s);
+    return out;
+
+
+}
+int main(int argc, char *argv[]){
+    struct option longOpts []={
+        {"Unew_ulimit",optional_argument,0,'U'},
+        {"Csize",required_argument,0,'C'},
+        {"Vname",required_argument,0,'V'}
+    };
+    char opt;
+    while((opt=getopt_long(argc,argv,"ispucdv",longOpts,NULL))!=-1){
+        switch(opt){
+            case 'i':
+                struct GUID GUID;
+                GUID = getGUID();
+                printf("UID:%d\nEUID:%d\nGID:%d\nEGID:%d\n",GUID.uid,GUID.euid,GUID.gid,GUID.egid);
+                break;
+            default:break;
+        }
+    }
+    
 }
