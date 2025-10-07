da0ca3f445f5ed5be08ccaf6bb1f7efb183f202d
Eugen <kirillov2255@yandex.ru>
2025-10-07 13:10:22 +0700

    fix 2nd task

--- e.kirillov/task2/task2.c
+++ e.kirillov/task2/task2.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+
+
 extern char *tzname[];
 
 int main()
@@ -20,7 +22,7 @@ int main()
     sp = localtime(&now);
     printf("%d/%d/%02d %d:%02d %s\n",
         sp->tm_mon + 1, sp->tm_mday,
-        sp->tm_year, sp->tm_hour,
+        sp->tm_year + 1900, sp->tm_hour,
         sp->tm_min, tzname[sp->tm_isdst]);
     
     exit(0);
