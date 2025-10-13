fd27eb56c913b6e9456c74377b66034785442fe6
Sambuca72 <artemstolyarov91567@gmail.com>
2025-10-13 22:24:29 +0700

    f

--- stolyarov/lab4/prog.c
+++ stolyarov/lab4/prog.c
@@ -1,90 +1,74 @@
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-
-#define BUFFER_SIZE 256
-
-
-typedef struct Node_s {
-    char *string;
-    struct Node_s *next;
-} Node;
-
-Node *head, *tail;
-
-
-void push(char *string) {
-    unsigned long len = strlen(string) + 1;
-    char *copyPtr = calloc(len, sizeof(char));
-    strcpy(copyPtr, string);
-
-    tail->string = copyPtr;
-    tail->next = calloc(1, sizeof(Node));
-
-    tail = tail->next;
-}
-
-void printList() {
-    for(Node *ptr = head; ptr != NULL; ptr = ptr->next)
-        if (ptr->string) 
-            printf("%s\n", ptr->string);
-}
-
-int main() {
-    char inputBuf[BUFFER_SIZE] = {0};
-
-    // инициализация
-    head = malloc(sizeof(Node));
-
-    head->string = NULL;
-    head->next = NULL;
-
-    tail = head;
-
-    while (fgets(inputBuf, BUFFER_SIZE, stdin) != NULL) {
-        // конец чтения
-        if (inputBuf[0] == '.') {
-            printList();
-            return 0;
-        }
-
-        // находим позицию \n
-        char *lineEnd = strchr(inputBuf, '\n');
-
-        // нет \n => длинная строка
-        if (lineEnd == NULL) {
-            size_t newBufCnt = 0;
-            size_t newBufCap = BUFFER_SIZE;
-            char *newBuf = malloc(BUFFER_SIZE);
-
-            memcpy(newBuf, inputBuf, BUFFER_SIZE);
-            newBufCnt += BUFFER_SIZE - 1;
-
-            // продолжаем читать строку
-            while (fgets(inputBuf, BUFFER_SIZE, stdin) != NULL) {
-                // увеличиваем общий размер строки
-                newBufCap += BUFFER_SIZE;
-                newBuf = realloc(newBuf, newBufCap);
-
-                memcpy(newBuf + newBufCnt, inputBuf, BUFFER_SIZE);
-                newBufCnt += BUFFER_SIZE - 1;
-
-                // если находим \n конец строки
-                lineEnd = strchr(newBuf, '\n');
-                if (lineEnd) {
-                    *lineEnd = '\0';
-                    push(newBuf);
-                    free(newBuf);
-                    break;
-                }
+void remove_function_key_sequences(char *str) {
+    char *src = str;
+    char *dst = str;
+    
+    while (*src) {
+        if (*src == 27 || (*src == '^' && *(src + 1) == '[')) {
+            if (*src == 27) {
+                src++;
+            } else {
+                src += 2;
+            }
+            
+            while (*src && 
+                   ((*src >= 'A' && *src <= 'Z') ||
+                    (*src >= '0' && *src <= '9') ||
+                    *src == '[' || *src == '~' || *src == ';' ||
+                    *src == 'O' || *src == 'P' || *src == 'Q' || 
+                    *src == 'R' || *src == 'S')) {
+                src++;
             }
         } else {
-            *lineEnd = '\0';
-            push(inputBuf);
+            *dst++ = *src++;
+        }
+    }
+    *dst = '\0';
+}
+
+
+int main(){
+
+    char** arr=NULL;
+    char str[1000];
+    int count=0;
+
+    while(fgets(str,1000,stdin))
+    {
+
+        remove_function_key_sequences(str);
+
+        if(str[0]!='.')
+        {
+            char **temp = (char**)realloc(arr,(count+1)*sizeof(char*));
+
+            if(temp==NULL){
+                perror("Error realloc");
+                break;
+            }
+
+            temp[count++]=strdup(str);
+
+            arr=temp;
+        }
+        else
+        {
+            break;
         }
     }
 
+    printf("\nВведённые строки:\n");
+
+    for(int i=0;i<count;i++)
+    {
+        printf("%s",arr[i]);
+        free(arr[i]);
+    }
+
+    free(arr);
+
     return 0;
 }
-
