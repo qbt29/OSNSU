885f08f762705c75baa2affe07facfa8937f33f4
Eugen <kirillov2255@yandex.ru>
2025-10-14 13:04:28 +0700

    fix 7 task

--- e.kirillov/task7/task7.c
+++ e.kirillov/task7/task7.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <stdio.h>
-#include <sys/select.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
 
 typedef struct {
     off_t offset;
@@ -15,6 +17,12 @@ typedef struct {
     int cnt;
     int cap;
 } Array;
+
+volatile sig_atomic_t timeout_occurred = 0;
+
+void alarm_handler(int sig) {
+    timeout_occurred = 1;
+}
 
 void push(Array *a, Line element) {
     if (a->cnt == a->cap) {
@@ -32,6 +40,16 @@ void printLine(Line line, const char *mapped) {
     printf("\n");
 }
 
+void print_all_lines_and_exit(Array *table, const char *mapped, size_t size) {
+    printf("\n");
+    for (int i = 0; i < table->cnt; i++)
+        printLine(table->array[i], mapped);
+    
+    munmap((void *)mapped, size);
+    free(table->array);
+    exit(0);
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 2)
         return 1;
@@ -47,14 +65,19 @@ int main(int argc, char *argv[]) {
         return 1;
 
     struct stat fileInfo;
-    if (fstat(file, &fileInfo) == -1)
+    if (fstat(file, &fileInfo) == -1) {
+        close(file);
         return 1;
+    }
 
     size_t size = fileInfo.st_size;
 
     const char *mapped = mmap(NULL, size, PROT_READ, MAP_PRIVATE, file, 0);
-    if (mapped == MAP_FAILED)
+    if (mapped == MAP_FAILED) {
+        close(file);
         return 1;
+    }
+    close(file);
 
     off_t lineOffset = 0;
     off_t lineLength = 0;
@@ -76,32 +99,67 @@ int main(int argc, char *argv[]) {
         push(&table, current);
     }
 
-    fd_set fdset;
-    struct timeval timeout;
+    // Установка обработчика сигнала
+    signal(SIGALRM, alarm_handler);
 
+    // Сохраняем оригинальные флаги stdin
+    int old_flags = fcntl(STDIN_FILENO, F_GETFL);
+    // Устанавливаем неблокирующий режим
+    fcntl(STDIN_FILENO, F_SETFL, old_flags | O_NONBLOCK);
+    alarm(5);
     while (1) {
+        timeout_occurred = 0;
         printf("Enter the line number: ");
         fflush(stdout);
 
-        FD_ZERO(&fdset);
-        FD_SET(0, &fdset); // STDIN_FILENO = 0
-        timeout.tv_sec = 5;
-        timeout.tv_usec = 0;
+        
 
-        if (!select(1, &fdset, NULL, NULL, &timeout)) {
-            printf("\n\n");
-            for (int i = 0; i < table.cnt; i++)
-                printLine(table.array[i], mapped);
-            return 0;
+        char temp[123] = {0};
+        int pos = 0;
+        
+        // Чтение посимвольно с проверкой таймаута
+        while (pos < sizeof(temp) - 1) {
+            if (timeout_occurred) {
+                print_all_lines_and_exit(&table, mapped, size);
+            }
+            
+            char ch;
+            int bytes_read = read(STDIN_FILENO, &ch, 1);
+            
+            if (bytes_read > 0) {
+                if (ch == '\n') {
+                    temp[pos] = '\0';
+                    break;
+                }
+                temp[pos++] = ch;
+            } else if (bytes_read == 0) {
+                // EOF
+                break;
+            } else if (errno != EAGAIN && errno != EWOULDBLOCK) {
+                // Ошибка чтения
+                break;
+            }
+            
+            // Небольшая задержка чтобы не грузить CPU
+            usleep(10000); // 10ms
         }
 
-        char temp[123];
-        printf("Enter the line number: ");
-        scanf("%s", temp);
+        // Отмена таймера
+        alarm(0);
+
+        // Если ничего не введено и был таймаут
+        if (timeout_occurred) {
+            print_all_lines_and_exit(&table, mapped, size);
+        }
+
+        // Если ничего не введено
+        if (pos == 0) {
+            break;
+        }
 
         int num = atoi(temp);
 
-        if(atoi(temp) == 0)
+        if (num == 0)
             break;
             
         if (table.cnt < num) {
@@ -113,11 +171,8 @@ int main(int argc, char *argv[]) {
         printLine(line, mapped);
     }
 
-    munmap((void *) mapped, size);
+    munmap((void *)mapped, size);
     free(table.array);
-    table.array = NULL;
-    table.cnt = 0;
-    table.cap = 0;
 
     return 0;
 }
