3a9e8d1101009ad3dea07aedd94ec36af58e34e5
Артём <artemstolyarov91567@gmail.com>
2025-10-14 13:11:20 +0700

    Update prog.c

--- stolyarov/lab4/prog.c
+++ stolyarov/lab4/prog.c
@@ -2,12 +2,10 @@
 #include <string.h>
 #include <stdlib.h>
 
-// структура узла списка
 typedef struct Node {
     char* data;
     struct Node* next;
 } Node;
-
 
 void delete_wrong_sumbols(char* str)
 {
@@ -34,7 +32,7 @@ void delete_wrong_sumbols(char* str)
             }
         }
         else if (str[i] == 27)
-        { 
+        { // ESC символ (^[)
             in_escape = 1;
             escape_start = i;
         }
@@ -55,38 +53,53 @@ void delete_wrong_sumbols(char* str)
 int main() {
     const size_t MAX_LEN_STR = 1024;
 
+    // инициализируем голову и хвост списка
     Node* head = NULL;
     Node* tail = NULL;
 
+    // инициализируем строку максимально возможной длины
     char* in_str = (char*)malloc(sizeof(char) * MAX_LEN_STR);
     if (in_str == NULL) { fprintf(stderr, "Ошибка выделения памяти!\n"); return 1; }
 
     printf("\nВведите строки для записи в список.\n");
+    printf("Строка с начальным символом <.> завершает ввод и выводит все содержимое:\n");
+
     while (1) {
+        // пока можем считывать, считываем
         printf("Ваша строка: ");
         if (fgets(in_str, MAX_LEN_STR, stdin) == NULL) { break; }
 
+        // длина строки
         size_t len_in_str = strlen(in_str);
 
+        // заменяем последний символ
         if (in_str[len_in_str-1] == '\n') {
             in_str[len_in_str-1] = '\0';
             len_in_str--;
         }
 
+        // если начинается с точки => прерываем
         if (len_in_str > 0 && in_str[0] == '.') { break; }
+
+        // удаление символов
+        // delete_wrong_sumbols(in_str);
         
+        // обновляем длину строки после фильтрации
         len_in_str = strlen(in_str);
 
+        // перезаписываем в новую строку
         char* str = (char*)malloc(sizeof(char) * (len_in_str + 1));
         if (str == NULL) { fprintf(stderr, "Ошибка выделения памяти!\n"); return 1; }
         strcpy(str, in_str);
 
+        // создаем новый узел списка
         Node* new_node = (Node*)malloc(sizeof(Node));
         if (new_node == NULL) { fprintf(stderr, "Ошибка выделения памяти!\n"); return 1; }
 
         new_node->data = str;
         new_node->next = NULL;
 
+        // добавляем узел в конец списка
         if (head == NULL) {
             head = new_node;
             tail = new_node;
@@ -98,6 +111,7 @@ int main() {
         memset(in_str, 0, MAX_LEN_STR * sizeof(char));
     }
 
+    // последовательно выводим строки из списка И сразу очищаем память
     Node* current = head;
     while (current != NULL) {
         printf("%s\n", current->data);
